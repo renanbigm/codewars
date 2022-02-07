2c6a4e8d83886b54866574ec79fc29e59291816d
Renan Fagundes <renanbigm@gmail.com>
2022-02-07 06:43:35 -0300

    function highAndLows
    econtra o maior e menor elemento de uma string

---
+++ outro.js
@@ -0,0 +1,11 @@
+function highAndLow(numbers){
+  
+    let elements = numbers.split(" ");
+    let result = [];
+  
+    for (let i = 0; i < elements.length; i++) {
+      result.push(elements[i]); 
+    }
+    return Math.max(...result) + " " + Math.min(...result); 
+  }
+  

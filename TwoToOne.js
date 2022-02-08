f7c1d9dc307c6cdaca588711386d4d57c8d54292
renanbigm <80122856+renanbigm@users.noreply.github.com>
2022-02-08 10:30:38 -0300

    Update TwoToOne.js

--- TwoToOne.js
+++ TwoToOne.js
@@ -1,10 +1,8 @@
-function longest(s1, s2) {
-   
-  let fullS = s1 + s2;
-  let mySet = new Set(fullS);
-  let phrase = mySet.join("");
- 
-return mySet.join("")
+function longest(s1, s2){
+  let conc = s1 + s2; // soma das duas strings
+  let newA = conc.split(""); // transforma cada elemento da string em um array
+  let mySet = [...new Set(newA)]; // retira os elementos repetidos e mantem a forma de array 
+  let sequence = mySet.sort();  // poe os elementos em sequência
+  let result = sequence.join(""); // transforma o array em string.
+  return result;
 }
-longest("ui", "ueh")
-

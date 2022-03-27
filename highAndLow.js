908438d4ed6791819c23c44ce3f0514390ebcbdf
renanbigm <renanbigm@gmail.com>
2022-03-26 22:51:33 -0300

    executando a funçao

--- highAndLow.js
+++ highAndLow.js
@@ -1,11 +1,14 @@
 function highAndLow(numbers){
   
-    let elements = numbers.split(" ");
-    let result = [];
+  let elements = numbers.split(" ");
+  let result = [];
   
-    for (let i = 0; i < elements.length; i++) {
-      result.push(elements[i]); 
-    }
-    return Math.max(...result) + " " + Math.min(...result); 
+  for (let i = 0; i < elements.length; i++) {
+    result.push(elements[i]); 
   }
+  return Math.max(...result) + " " + Math.min(...result); 
+}
+
+
+
   
